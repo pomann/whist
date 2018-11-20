019da96e66dd1b32c4632911e33d0fbf61d92ad6
JamieHyland1 <jamiehyland01@gmail.com>
2018-11-20 15:37:26 +0000

    added player name code

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,28 @@
-//self explanatory here, just setting up canvas size and making sure its drawn in the right place
-function setup() {
-  // put setup code here
-	var c = createCanvas(500,400)
-	var canvasDiv = document.getElementById("canvas")
-	c.parent(canvasDiv)
-	background(0)
+//Get the player name from the input field and send it to the server
+var socket = io.connect();
+var playerName
+//this function is automatically called whenever the button on the page is clicked
+function enterName() {
+	var n = document.getElementById("name").value
+	socket.emit("set_username", n)
+	playerName = n
+
 }
 
+//self explanatory here, just setting up canvas size and making sure its drawn in the right place
+function setup() {
+	// put setup code here
+	var c = createCanvas(500, 400)
+	var canvasDiv = document.getElementById("canvas")
+	c.parent(canvasDiv)
+	background(7, 99, 36)
+	fill(255)
 
+
+}
 
 //This draw function runs every frame
 function draw() {
-	//messing around
-  var r = random(255)
-  var g = random(255)
-  var b = random(255)
- 	
-  fill(r,g,b)
-  ellipse(random(500), random(400), 50, random(40,55));
+	text(playerName, 10, 10)
+
 }
