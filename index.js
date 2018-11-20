9f42fa68a1528a39c2ae0fcbcfd02da9d9dc1233
omo <omo1812@gmail.com>
2018-11-20 23:11:19 +0000

    Added lobby class to server

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+//
+//
+//
+//   _____   ____  _   _ _ _______   _______ ____  _    _  _____ _    _   _______ _    _ _____  _____   ______ _____ _      ______ 
+//  |  __ \ / __ \| \ | ( )__   __| |__   __/ __ \| |  | |/ ____| |  | | |__   __| |  | |_   _|/ ____| |  ____|_   _| |    |  ____|
+//  | |  | | |  | |  \| |/   | |       | | | |  | | |  | | |    | |__| |    | |  | |__| | | | | (___   | |__    | | | |    | |__   
+//  | |  | | |  | | . ` |    | |       | | | |  | | |  | | |    |  __  |    | |  |  __  | | |  \___ \  |  __|   | | | |    |  __|  
+//  | |__| | |__| | |\  |    | |       | | | |__| | |__| | |____| |  | |    | |  | |  | |_| |_ ____) | | |     _| |_| |____| |____ 
+//  |_____/ \____/|_| \_|    |_|       |_|  \____/ \____/ \_____|_|  |_|    |_|  |_|  |_|_____|_____/  |_|    |_____|______|______|
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
 //these are basically the modules we need to input the app, think of them as imports from python
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var players = {};
 var soc = {};
+var lobbies = {};
 var path = require('path')
 var express = require('express')
 
@@ -15,6 +44,34 @@ class Player {
 		this.hand;
 	}
 }
+
+class Lobby {
+	constructor(){
+	    this.lobbyName;
+		this.waitingPlayers = [];
+		this.empty = false;
+	}
+
+	fullLobby(){
+		if(this.waitingPlayers.length == 4){
+		    delete lobbies[this.lobbyName];
+		    this.start();
+		}
+	}
+	
+	addPlayer(player){
+	    this.waitingPlayers.push(player);
+	}
+
+	start(){
+		//starts the game once the lobby is full
+	}
+
+	deleteLobby(){
+		//deletes the lobby if it's empty
+	}
+}
+
 
 //this will send the index.html file to the client when a GET request is sent to the server
 //read this if you wanna know more about whats going on, it'll help if you have to debug later
@@ -53,10 +110,25 @@ io.on('connection', function(socket){
     	}
     	console.log(players);
     });
-    
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit("msg", msg);
+	
+	socket.on("create_lobby", function(lobbyName) {
+	    if(lobbyName in lobbies){
+	        socket.emit("accept_lobby", false);
+	    }else{
+	        lobbies[lobbyName] = new Lobby();
+	        lobbies[lobbyName].addPlayer(socket.id);
+	        socket.emit('accept_lobby', true);
+	    }
+	});
+	
+	socket.on("join_lobby", function(lobbyName) {
+	    if(lobbyName in lobbies){
+	        lobbies[lobbyName].addPlayer(socket.id);
+	        lobbies[lobbyName].fullLobby();
+	        socket.emit("joined_lobby", true);
+	    }else{
+	        socket.emit("joined_lobby", false);
+	    }
 	});
 });
 //set the server to listen on the port 3000
