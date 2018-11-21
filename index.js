f816571500f101aecd06673c25419bede2379dec
omo <omo1812@gmail.com>
2018-11-21 22:59:55 +0000

    improved server DONT TOUCH

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var players = {};
 var soc = {};
-var lobbies = {};
 var path = require('path')
 var express = require('express')
 
@@ -42,12 +41,14 @@ class Player {
 		this.id = id;
 		this.playerName;
 		this.hand;
+		this.inLobby;
+		this.playing = false;
 	}
 }
 
 class Lobby {
-	constructor(){
-	    this.lobbyName;
+	constructor(lobbyName){
+	    this.lobbyName = lobbyName;
 		this.waitingPlayers = [];
 		this.empty = false;
 	}
@@ -61,17 +62,30 @@ class Lobby {
 	
 	addPlayer(player){
 	    this.waitingPlayers.push(player);
+	    this.fullLobby()
+	}
+	
+	deletePlayer(playerId){
+		this.waitingPlayers.splice(this.waitingPlayers.indexOf(playerId),1);
+		this.deleteLobby();
 	}
 
 	start(){
-		//starts the game once the lobby is full
+		for(var i = 0;i<this.waitingPlayers.length;i++){
+			players[soc[this.waitingPlayers[i]]].playing = true;
+			io.sockets.connected[this.waitingPlayers[i]].emit('start_game', true);
+		}
 	}
 
 	deleteLobby(){
 		//deletes the lobby if it's empty
+		if(this.waitingPlayers.length == 0){
+			delete lobbies[this.lobbyName]
+		}
 	}
 }
 
+var lobbies = {"lobby1": new Lobby("lobby1")};
 
 //this will send the index.html file to the client when a GET request is sent to the server
 //read this if you wanna know more about whats going on, it'll help if you have to debug later
@@ -88,8 +102,12 @@ io.on('connection', function(socket){
     console.log('a user connected');
     console.log(socket.id);
     soc[socket.id] = "";
+    
     socket.on('disconnect', function(){
         if(soc[socket.id] != ""){
+        	if(players[soc[socket.id]].inLobby != "" && !players[soc[socket.id]].playing){
+        		lobbies[players[soc[socket.id]].inLobby].deletePlayer(socket.id);
+        	}
         	delete players[soc[socket.id]];
         }
         delete soc[socket.id];
@@ -101,12 +119,15 @@ io.on('connection', function(socket){
     socket.on('set_username', function(username){
     	if(username in players){
     		socket.emit("accept_username",false);
-    	}else{
+		}else{
     		soc[socket.id] = username;
     		players[soc[socket.id]] = new Player(socket.id);
     		players[soc[socket.id]].playerName = username;
         	console.log(username);
         	socket.emit("accept_username",true);
+        	for(var key in lobbies) {
+    			socket.emit("lobbies",key);
+        	}
     	}
     	console.log(players);
     });
@@ -115,17 +136,18 @@ io.on('connection', function(socket){
 	    if(lobbyName in lobbies){
 	        socket.emit("accept_lobby", false);
 	    }else{
-	        lobbies[lobbyName] = new Lobby();
+	        lobbies[lobbyName] = new Lobby(lobbyName);
 	        lobbies[lobbyName].addPlayer(socket.id);
+	        players[soc[socket.id]].inLobby = lobbyName;
 	        socket.emit('accept_lobby', true);
 	    }
 	});
 	
 	socket.on("join_lobby", function(lobbyName) {
 	    if(lobbyName in lobbies){
-	        lobbies[lobbyName].addPlayer(socket.id);
-	        lobbies[lobbyName].fullLobby();
+	        players[soc[socket.id]].inLobby = lobbyName;
 	        socket.emit("joined_lobby", true);
+	        lobbies[lobbyName].addPlayer(socket.id);
 	    }else{
 	        socket.emit("joined_lobby", false);
 	    }
